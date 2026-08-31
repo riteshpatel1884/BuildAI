7e805ff0aea8f1dfaef011803373c684a2a5e1cc
ritesh1884 <riteshpatel1884@gmail.com>
2026-08-31 23:44:29 +0530

    addes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css';
 import { ThemeProvider } from './ThemeProvider';
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,11 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col"><ThemeProvider>{children}</ThemeProvider></body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider>{children}</ThemeProvider>
+         <Analytics />
+         <SpeedInsights />
+      </body>
     </html>
   );
 }
