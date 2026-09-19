eddc25a3d4f0a9d6833d0d58c5e90a8f26b8b5e7
ritesh1884 <riteshpatel1884@gmail.com>
2026-09-19 23:12:12 +0530

    Linear regression code

--- src/app/machine-learning/docs.js
+++ src/app/machine-learning/docs.js
@@ -7,7 +7,11 @@ export const MACHINE_LEARNING = {
 
       notes:
         'https://docs.google.com/document/d/1IubqOSElgcLc4-bT6rPgfXtOz08lgD4Jd2KftmGC8e0/edit?tab=t.j9yh3lpzm823',
-
+      code: {
+              repo: 'riteshpatel1884/AIML',
+              path: 'ML/01_Car_Price_Prediction',
+              branch: 'main',
+            },
     },
   },
 };
